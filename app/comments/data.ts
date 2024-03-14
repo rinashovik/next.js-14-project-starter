7fb1d6d6184326d82b3fb3500a72e0fb4af35913
@aweSomeCode <57203336+rinashovik@users.noreply.github.com>
2024-03-14 13:44:32 -0500

    get-post-method

---
+++ app/comments/data.ts
@@ -0,0 +1,10 @@
+export const posts = [
+    {id: 1, 
+    text: "This is the first Post"
+},
+    {id: 2, 
+    text: "This is the second Post"
+},
+    {id: 3, 
+    text: "This is the third Post"}
+]
